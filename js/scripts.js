5f1db6836460fbac5d6a1a5a8365ce4d26c48c63
Katie Reed <katharynreedpierce@gmail.com>
2017-01-20 09:54:04 -0800

    create if/else statement in JS

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@ var pingOrPongString = "";
 var ping = "ping"
 var pong = "pong"
 var pingPong = "pingPong"
+
+
+var convert = function(userInput) {
+  if (userInput % 3) {
+    return ping;
+  } else if (userInput % 5) {
+    return pong;
+  } else if (userInput % 15) {
+    return pingPong;
+  };
+};
