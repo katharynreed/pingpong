7cc3de9d40aa5b31ae970d6e42575114b59e7302
Katie Reed <katharynreedpierce@gmail.com>
2017-01-20 13:13:10 -0800

    add styling to background image and change header text

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@ var pingOrPongString = "";
 var divide = function(userInput) {
   for(var i = 1; i <= userInput; i++) {
     if (i % 15 === 0) {
-      $("#pingpong-results").append("<p>"+i+": pingPong</p>");
+      $("#pingpong-results").append("<p>"+i+" ⤏ pingPong</p>");
     } else if (i % 5 === 0) {
-      $("#pingpong-results").append("<p>"+i+": pong</p>");
+      $("#pingpong-results").append("<p>"+i+" ⤏ pong</p>");
     } else if (i % 3 === 0) {
-      $("#pingpong-results").append("<p>"+i+": ping</p>");
+      $("#pingpong-results").append("<p>"+i+" ⤏ ping</p>");
     } else {
       $("#pingpong-results").append("<p>"+i+"</p>");
     }
